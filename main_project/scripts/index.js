55c4d10b6ebc6937710f3a4f8b2edb60a632ec4e
Trevor Stephenson <trevors19themail@gmail.com>
2025-03-20 13:55:28 -0600

    Fixed js api keys a bit

--- main_project/scripts/index.js
+++ main_project/scripts/index.js
@@ -1,7 +1,13 @@
 var esriConfig = {
-  apiKey: API_KEY
+  apiKey: API_KEY,
+  nationalParksKey: NATIONAL_API_KEY
 };
 
-console.log("ArcGIS map script loaded!");
+async function fetchParks() {
+  const response = await fetch(
+    `https://developer.nps.gov/api/v1/parks?limit=500&api_key=${esriConfig.nationalParksKey}`
+  );
 
-  
+  const data = await response.json();
+  return data.data;
+}
