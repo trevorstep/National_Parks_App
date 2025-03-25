996948dda82670880da939ebbfe474bdf5e708ae
Trevor Stephenson <trevors19themail@gmail.com>
2025-03-25 13:01:56 -0600

    com

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Allow requests from specific origin
-const allowedOrigins = ['https://trevorstep.github.io/National_Parks_App/main_project/main.html']; // Update with your specific domains
+const allowedOrigins = ['https://trevorstep.github.io/National_Parks_App/main_project/main.html']; 
 const options = {
   origin: function(origin, callback) {
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
