5478ad30f4026c6e3689c8ed877d826900f16f86
Trevor Stephenson <trevors19themail@gmail.com>
2026-02-26 01:52:39 -0700

    Try this

--- main_project/scripts/index.js
+++ main_project/scripts/index.js
@@ -58,6 +58,57 @@ async function fetchParks() {
   }
 }
 
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+if (gallery) {
+  gallery.addEventListener('click', openModal);
+}
+
+if (modal) {
+  closeButton.addEventListener('click', () => {
+    modal.close();
+  });
+
+  modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+  });
+}
+
+function openModal(e) {
+  if (e.target.tagName === 'IMG') {
+    const img = e.target;
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+    const full = src.replace('low', 'high');
+
+    modalImage.src = full;
+    modalImage.alt = alt;
+        
+    modal.showModal();
+  }
+}
+
+const btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('nav');
+
+if (btn) {
+  btn.addEventListener('click', togglemenu);
+}
+
+function togglemenu() {
+    if (menu) {
+      menu.classList.toggle('hide');
+    }
+    if (btn) {
+      btn.classList.toggle('change');
+    }
+}
+
 function createPopupContent(attributes) {
   const container = document.createElement('div');
   container.className = 'popup-content';
@@ -113,73 +164,6 @@ function createPopupContent(attributes) {
       container.appendChild(hiddenDiv);
     }
   }
-
-
-
-
-
-
-
-
-
-
-
-
-const gallery = document.querySelector('.gallery');
-const modal = document.querySelector('dialog');
-const modalImage = modal.querySelector('img');
-const closeButton = modal.querySelector('.close-viewer');
-
-gallery.addEventListener('click', openModal);
-
-function openModal(e) {
-console.log(e.target);    
-const img =e.target;
-
-const src = img.getAttribute('src');
-const alt=img.getAttribute('alt');
-const full= src.replace('sm','full');
-
-modalImage.src = full;
-modalImage.alt = alt;
-    
-modal.showModal();
-}
-closeButton.addEventListener('click', () => {
-    modal.close();
-});
-
-// Close modal if clicking outside the image
-modal.addEventListener('click', (event) => {
-    if (event.target === modal) {
-        modal.close();
-    }
-});
-
-let btn = document.querySelector('.menu-btn');
-const menu = document.querySelector('nav');
-
-
-btn.addEventListener('click', togglemenu);
-
-function togglemenu() {
-    menu.classList.toggle('hide');
-    btn.classList.toggle('change');
-}
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   const description = document.createElement('p');
   description.className = 'popup-description';
